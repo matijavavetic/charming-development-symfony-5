75493057882fc26e31d664ae5b76da4f9aa44d2a
Matija <matija.vavetic@gmail.com>
2020-10-09 14:10:29 +0200

    Typehinting Symfony services

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,13 @@ class CommentController extends AbstractController
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(0, 5);
         }
 
